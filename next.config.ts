980ba486cfcf034092c2017964d0496bc7ccd492
tarek <tarek.ramadan2008@gmail.com>
2025-08-11 21:49:49 +0300

    init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+ 
+import withSerwistInit from '@serwist/next';
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const withSerwist = withSerwistInit({
+  swSrc: 'src/app/sw.ts',
+   
+  swDest: "public/sw.js",
+  cacheOnNavigation: true,
+  additionalPrecacheEntries: [{ url: "/offline", revision: '11' },
+    { url: "/offlinedoc", revision: '12' }],
+});
 
-export default nextConfig;
+export default withSerwist({
+  
+});
